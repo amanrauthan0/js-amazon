da9dd075acd999b826643d8fa2676d7f759f198a
Amansingh <aman44444490@gmail.com>
2025-12-23 10:41:24 +0530

    Add the starting files

---
+++ data/cart.js
@@ -0,0 +1,45 @@
+export let cart=JSON.parse(localStorage.getItem('cart'));
+
+if(!cart){
+  cart=[{
+  productId:'54e0eccd-8f36-462b-b68a-8182611d9add',
+  quantity:2
+},{
+  productId:'3ebe75dc-64d2-4137-8860-1f5a963e534b',
+  quantity:2
+}];
+}
+
+
+
+function saveToStorage(){
+  localStorage.setItem("cart",JSON.stringify(cart))
+}
+
+export function deletefromcart(productId){
+    const newcart=[];
+    cart.forEach((cartitem)=>{
+        if(productId!==cartitem.productId){
+          newcart.push(cartitem);
+        }
+    })
+    cart=newcart;
+    saveToStorage();
+}
+export function addtocart(productId){
+  let matchingitem;
+    cart.forEach((cartitem)=>{
+      if(productId===cartitem.productId){
+        matchingitem=cartitem;
+      }
+    })
+    if(matchingitem){
+      matchingitem.quantity+=1;
+    }else{
+      cart.push({
+      productId:productId,
+      quantity:1
+    })
+    }
+    saveToStorage();
+}
